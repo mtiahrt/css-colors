8aff3abbdf6439184f006992bb001a3127a040b2
mtiahrt <mark.tiahrt@outlook.com>
2021-09-21 12:04:44 -0600

    initial comment.  getting csv data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useCallback, useEffect} from 'react';
 import './App.css';
+import {csv} from 'd3';
+
+const csvUrl = 'https://gist.githubusercontent.com/mtiahrt/843efa70f24224397381470e0fc741c2/raw/cssNameColors.csv';
 
 function App() {
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    csv(csvUrl).then(data => {
+      console.log('fetching data');
+      setData(data);
+    })
+  }, [])
+
+  const message = data => {
+    let message = '';
+    message += data.columns.length + ' columns'
+    return message;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Data is: {data ? message(data) : 'loading'}
     </div>
   );
 }
