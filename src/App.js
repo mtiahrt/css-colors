01453b7a4180f34af1dfa15b96fb9e7134725de6
mtiahrt <mark.tiahrt@outlook.com>
2021-09-21 13:57:22 -0600

    completed pie chart spectrum

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import {csv} from 'd3';
+import {csv, arc, pie} from 'd3';
 
 const csvUrl = 'https://gist.githubusercontent.com/mtiahrt/843efa70f24224397381470e0fc741c2/raw/cssNameColors.csv';
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const pieArc = arc()
+    .innerRadius(0)
+    .outerRadius(width)
 
 function App() {
   const [data, setData] = useState(null);
   
   useEffect(() => {
     csv(csvUrl).then(data => {
-      console.log('fetching data');
       setData(data);
     })
-  }, [])
+  }, []);
 
-  const message = data => {
-    let message = '';
-    message += data.columns.length + ' columns'
-    return message;
+  if(!data){
+    return <pre>Loading...</pre>
   }
-  return (
-    <div className="App">
-      Data is: {data ? message(data) : 'loading'}
-    </div>
-  );
+
+  return <svg width={width} height={height}>
+    <g transform={`translate(${centerX}, ${centerY})`}>
+      {/* d3 pie function is magic to me.  need to reasearch more */}
+      {pie().value(1)(data).map((item, i) => (
+        <path fill = {item.data['RGB hex value']}
+        d={pieArc(item)}
+      />
+      ))};
+    </g>
+  </svg> 
 }
 
 export default App;
